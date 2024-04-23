7269babc75eac474d0e2809b5b9d6aa9512a45e3
laurynas-cyber <58857487+laurynas-cyber@users.noreply.github.com>
2024-04-24 00:36:33 +0300

    switch players

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const everyField = document.querySelectorAll(".flex div");
 let XorO = true;
 
 everyField.forEach((f) => {
+
   f.addEventListener("click", () => {
+    console.log(XorO);
     if (XorO == true) {
       f.style.backgroundColor = "red";
-      XorO = false;
+      return (XorO = false);
     }
     if (XorO == false) {
-      f.style.backgroundColor = "blue";
-      XorO = true;
+      f.style.backgroundColor = "gray";
+      return (XorO = true);
     }
   });
 });
